6060005905f5afc0f34cd5b5d2384b4509984ff8
ryuto ozawa <ryuto6747@gmail.com>
2025-05-23 09:57:13 +0900

    チャットルームとトップページがそれぞれ行き来できるように

--- chatSystem/src/main/java/com/example/demo/chat/ChatController.java
+++ chatSystem/src/main/java/com/example/demo/chat/ChatController.java
@@ -1,5 +1,18 @@
 package com.example.demo.chat;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ChatController {
 
+	@RequestMapping("/home")
+	public String pageTop() {
+		return "index";
+	}
+	
+	@RequestMapping("/view")
+	public String chatRoom() {
+		return "/chat/view";
+	}
 }
