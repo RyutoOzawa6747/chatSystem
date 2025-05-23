b6b44e8c6c59dc44f60eb733cd6bc7d58c446bcc
ryuto ozawa <ryuto6747@gmail.com>
2025-05-23 16:05:53 +0900

    チャットの編集・削除を可能に

--- chatSystem/src/main/java/com/example/demo/chat/ChatController.java
+++ chatSystem/src/main/java/com/example/demo/chat/ChatController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -47,4 +48,42 @@ public class ChatController {
 
 		return "redirect:/view";
 	}
+	
+	@RequestMapping("/admin")
+	public String edit(Model model) {
+		//データ一覧を取得
+		List<EntChat> list = chatdao.searchDb();
+		model.addAttribute("chatList",list);
+		
+		return "/chat/admin";
+	}
+	
+	@GetMapping("/del/{id}")
+	public String destroy(@PathVariable Long id) {
+		System.out.println("データの削除を実行");
+		chatdao.deleteDb(id);
+		
+		return "redirect:/admin";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Long id,Model model) {
+		//idからデータを特定
+		EntChat chat = chatdao.searchDbOne(id);
+		
+		model.addAttribute("chat",chat);
+		
+		return "chat/edit";
+	}
+	
+	@PostMapping("/edit/exe")
+	public String editExe(EntChat entchat) {
+		System.out.println("id:"+entchat.getId());
+		
+		//データの更新を実行
+		chatdao.updateDb(entchat);
+		return "redirect:/admin";
+	}
+	
+	
 }
--- chatSystem/src/main/java/com/example/demo/dao/ChatDao.java
+++ chatSystem/src/main/java/com/example/demo/dao/ChatDao.java
@@ -41,4 +41,29 @@ public class ChatDao {
 
 		return list;
 	}
+
+	public EntChat searchDbOne(Long id) {
+		//DBから探したい要素をMapで取得
+		Map<String, Object> dataDb = db.queryForMap("SELECT * FROM chat where id = ?", id);
+		//EntChat型に直す
+		EntChat data = new EntChat();
+		data.setId((int) dataDb.get("id"));
+		data.setName((String) dataDb.get("name"));
+		data.setComment((String) dataDb.get("comment"));
+
+		return data;
+	}
+
+	public void deleteDb(Long id) {
+		//もらったidで削除コマンド実行
+		db.update("delete from chat where id = ?", id);
+	}
+
+	public void updateDb(EntChat c) {
+		System.out.println("データを編集");
+		System.out.println("名前：" + c.getName());
+
+		db.update("update chat set name = ?, comment = ? where id = ?",
+				c.getName(), c.getComment(), c.getId());
+	}
 }
